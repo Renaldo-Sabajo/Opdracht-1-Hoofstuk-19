b4668e45c3cb8bf4f86cf9c34927e0f52e1bad46
Je-Melike <100957447+Je-Melike@users.noreply.github.com>
2022-03-10 10:41:34 -0300

    Update H9_Som1 .py

--- H9_Som1 .py
+++ H9_Som1 .py
@@ -21,28 +21,20 @@ def my_nth_root(x,n,tol):
     f = lambda y: (y**n) - x
     f_prime = lambda y: n * (y**(n-1))
     # y0 is een startwaarde, bijvoorbeeld
-    y0 = 1.5
+    y0 = 1.4
     n_r = y0 - (f(y0))/(f_prime(y0))
+    n > 1
+    x > 0
+    tol > 0
+    
     
     # Concept: new_n_r = n_r - (f(n_r))/(f_prime(n_r)) --> zo iets moeten we maken in recurion vorm
     # COncept: We moeten een subfunctie maken voor de recursie van de Newton Raphson formule
-    # Maar de nieuwe functie moet ook deel maken van my_nth_root
-    
-    def new_n_r(y0):
-        f = lambda y: (y**n) - x
-        f_prime = lambda y: n * (y**(n-1))
-        n_r = y0 - (f(y0))/(f_prime(y0))
-        if abs(f(y0)) < tol:
-            return y0
-        else:
-            return new_n_r(n_r)
-    d = new_n_r(n_r)
-    return d
-    
-#Voorbeeld (Zelfde gegevens als van dat boek) 
-print (my_nth_root(2,2,1e-6))
 
-  
+    if abs(f(y0)) < tol:
+        return y0
+    else:
+        return my_nth_root(x,n,tol)
 
 
 #Voorbeeld (Zelfde gegevens als van dat boek) 
